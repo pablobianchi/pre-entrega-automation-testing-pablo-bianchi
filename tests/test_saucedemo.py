cc7db5afefde709b2ad1652e45c54a8d4a050915
Pablo Bianchi <pablo.bianchi@diggo.agency>
2025-10-23 21:33:38 -0300

    FEAT:  Se agrega test_inventory

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,5 +1,11 @@
 import pytest
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 import time
 ##para poder utilizar lo que definí dentro de utils
 import sys
@@ -22,16 +28,25 @@ def driver():
 
 
 
-def test_login( driver):
-    login(driver)
+def test_login( driver ):
+    login(driver,True)
     assert "/inventory.html" in driver.current_url
 
 
-# def test_catalogo(driver):
-#     login(driver)
-#     take_screenshot(driver , 'inventario' );
-#     time.sleep(7)
-#     #assert "/inventory1.html" in driver.current_url
+def test_inventory(driver:webdriver.Chrome  ):
+
+    login(driver)
+    take_screenshot(driver , 'inventario' );
+
+    assert "Swag Labs" in driver.title , f"Título inesperado: {driver.title}"
+
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0 , f"No se encontraron productos"
+
+    assert len( driver.find_elements(By.CLASS_NAME, 'product_sort_container' ) ) > 0  , f"No se encontró elemento de Orden"
+
+    assert len( driver.find_elements(By.CLASS_NAME, 'bm-burger-button2' ) ) > 0  , f"No se encontró elemento de Menú"
 
 
 ##def test_carrito():
+##TODO
